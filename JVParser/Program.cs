2355993f1e5f9b96b3a6cfb7fb3309d5ef599bae
satomacoto <satomacoto@gmail.com>
2023-05-11 13:56:23 +0900

    Refactor to use recordClassMapping

--- JVParser/Program.cs
+++ JVParser/Program.cs
@@ -176,48 +176,50 @@ namespace JVParser
 
         static JVJson? JVReadToJson(string line, IEnumerable<string> skipRecordSpec)
         {
-            var av = new JV_AV_INFO();
-            var bn = new JV_BN_BANUSI();
-            var br = new JV_BR_BREEDER();
-            var bt = new JV_BT_KEITO();
-            var cc = new JV_CC_INFO();
-            var ch = new JV_CH_CHOKYOSI();
-            var ck = new JV_CK_CHAKU();
-            var cs = new JV_CS_COURSE();
-            var dm = new JV_DM_INFO();
-            var h1 = new JV_H1_HYOSU_ZENKAKE();
-            var h6 = new JV_H6_HYOSU_SANRENTAN();
-            var hc = new JV_HC_HANRO();
-            var hn = new JV_HN_HANSYOKU();
-            var hr = new JV_HR_PAY();
-            var hs = new JV_HS_SALE();
-            var hy = new JV_HY_BAMEIORIGIN();
-            var jc = new JV_JC_INFO();
-            var jg = new JV_JG_JOGAIBA();
-            var ks = new JV_KS_KISYU();
-            var o1 = new JV_O1_ODDS_TANFUKUWAKU();
-            var o2 = new JV_O2_ODDS_UMAREN();
-            var o3 = new JV_O3_ODDS_WIDE();
-            var o4 = new JV_O4_ODDS_UMATAN();
-            var o5 = new JV_O5_ODDS_SANREN();
-            var o6 = new JV_O6_ODDS_SANRENTAN();
-            var ra = new JV_RA_RACE();
-            var rc = new JV_RC_RECORD();
-            var se = new JV_SE_RACE_UMA();
-            var sk = new JV_SK_SANKU();
-            var tc = new JV_TC_INFO();
-            var tk = new JV_TK_TOKUUMA();
-            var tm = new JV_TM_INFO();
-            var um = new JV_UM_UMA();
-            var wc = new JV_WC_WOODCHIP();
-            var we = new JV_WE_WEATHER();
-            var wf = new JV_WF_INFO();
-            var wh = new JV_WH_BATAIJYU();
-            var ys = new JV_YS_SCHEDULE();
+            var recordClassMapping = new Dictionary<string, Type>
+            {
+                { "AV", typeof(JV_AV_INFO) },
+                { "BN", typeof(JV_BN_BANUSI) },
+                { "BR", typeof(JV_BR_BREEDER) },
+                { "BT", typeof(JV_BT_KEITO) },
+                { "CC", typeof(JV_CC_INFO) },
+                { "CH", typeof(JV_CH_CHOKYOSI) },
+                { "CK", typeof(JV_CK_CHAKU) },
+                { "CS", typeof(JV_CS_COURSE) },
+                { "DM", typeof(JV_DM_INFO) },
+                { "H1", typeof(JV_H1_HYOSU_ZENKAKE) },
+                { "H6", typeof(JV_H6_HYOSU_SANRENTAN) },
+                { "HC", typeof(JV_HC_HANRO) },
+                { "HN", typeof(JV_HN_HANSYOKU) },
+                { "HR", typeof(JV_HR_PAY) },
+                { "HS", typeof(JV_HS_SALE) },
+                { "HY", typeof(JV_HY_BAMEIORIGIN) },
+                { "JC", typeof(JV_JC_INFO) },
+                { "JG", typeof(JV_JG_JOGAIBA) },
+                { "KS", typeof(JV_KS_KISYU) },
+                { "O1", typeof(JV_O1_ODDS_TANFUKUWAKU) },
+                { "O2", typeof(JV_O2_ODDS_UMAREN) },
+                { "O3", typeof(JV_O3_ODDS_WIDE) },
+                { "O4", typeof(JV_O4_ODDS_UMATAN) },
+                { "O5", typeof(JV_O5_ODDS_SANREN) },
+                { "O6", typeof(JV_O6_ODDS_SANRENTAN) },
+                { "RA", typeof(JV_RA_RACE) },
+                { "RC", typeof(JV_RC_RECORD) },
+                { "SE", typeof(JV_SE_RACE_UMA) },
+                { "SK", typeof(JV_SK_SANKU) },
+                { "TC", typeof(JV_TC_INFO) },
+                { "TK", typeof(JV_TK_TOKUUMA) },
+                { "TM", typeof(JV_TM_INFO) },
+                { "UM", typeof(JV_UM_UMA) },
+                { "WC", typeof(JV_WC_WOODCHIP) },
+                { "WE", typeof(JV_WE_WEATHER) },
+                { "WF", typeof(JV_WF_INFO) },
+                { "WH", typeof(JV_WH_BATAIJYU) },
+                { "YS", typeof(JV_YS_SCHEDULE) }
+            };
 
 
             JObject? jsonObject = null;
-
             var recordSpec = line.Substring(0, 2);
 
             if (skipRecordSpec.Contains(recordSpec))
@@ -225,164 +227,22 @@ namespace JVParser
                 return null;
             }
 
-            switch (recordSpec)
-            {
-                case "AV":
-                    av.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(av);
-                    break;
-                case "BN":
-                    bn.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(bn);
-                    break;
-                case "BR":
-                    br.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(br);
-                    break;
-                case "BT":
-                    bt.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(bt);
-                    break;
-                case "CC":
-                    cc.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(cc);
-                    break;
-                case "CH":
-                    ch.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(ch);
-                    break;
-                case "CK":
-                    ck.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(ck);
-                    break;
-                case "CS":
-                    cs.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(cs);
-                    break;
-                case "DM":
-                    dm.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(dm);
-                    break;
-                case "H1":
-                    h1.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(h1);
-                    break;
-                case "H6":
-                    h6.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(h6);
-                    break;
-                case "HC":
-                    hc.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(hc);
-                    break;
-                case "HN":
-                    hn.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(hn);
-                    break;
-                case "HR":
-                    hr.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(hr);
-                    break;
-                case "HS":
-                    hs.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(hs);
-                    break;
-                case "HY":
-                    hy.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(hy);
-                    break;
-                case "JC":
-                    jc.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(jc);
-                    break;
-                case "JG":
-                    jg.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(jg);
-                    break;
-                case "KS":
-                    ks.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(ks);
-                    break;
-                case "O1":
-                    o1.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(o1);
-                    break;
-                case "O2":
-                    o2.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(o2);
-                    break;
-                case "O3":
-                    o3.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(o3);
-                    break;
-                case "O4":
-                    o4.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(o4);
-                    break;
-                case "O5":
-                    o5.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(o5);
-                    break;
-                case "O6":
-                    o6.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(o6);
-                    break;
-                case "RA":
-                    ra.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(ra);
-                    break;
-                case "RC":
-                    rc.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(rc);
-                    break;
-                case "SE":
-                    se.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(se);
-                    break;
-                case "SK":
-                    sk.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(sk);
-                    break;
-                case "TC":
-                    tc.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(tc);
-                    break;
-                case "TK":
-                    tk.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(tk);
-                    break;
-                case "TM":
-                    tm.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(tm);
-                    break;
-                case "UM":
-                    um.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(um);
-                    break;
-                case "WC":
-                    wc.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(wc);
-                    break;
-                case "WE":
-                    we.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(we);
-                    break;
-                case "WF":
-                    wf.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(wf);
-                    break;
-                case "WH":
-                    wh.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(wh);
-                    break;
-                case "YS":
-                    ys.SetDataB(ref line);
-                    jsonObject = JObject.FromObject(ys);
-                    break;
-                default:
-                    // 読み飛ばし
-                    break;
+            if (!recordClassMapping.ContainsKey(recordSpec)) {
+                return null;
             }
+
+            if (recordClassMapping.TryGetValue(recordSpec, out Type recordType))
+            {
+                var recordInstance = Activator.CreateInstance(recordType);
+                var setDataBMethod = recordType.GetMethod("SetDataB");
+
+                if (setDataBMethod != null)
+                {
+                    setDataBMethod.Invoke(recordInstance, new object[] { line });
+                    jsonObject = JObject.FromObject(recordInstance);
+                }
+            }
+
             if (jsonObject != null)
             {
                 var flattened = jsonObject
